797127c7e14e3a19784756107dad6e97d8385cfa
urbane0206 <maxime.tonnelier01@gmail.com>
2026-09-08 21:01:09 +0200

    Importing project

---
+++ Core/Src/gpio.c
@@ -0,0 +1,95 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(ALIM_TEMP_GPIO_Port, ALIM_TEMP_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(NSS_GPIO_Port, NSS_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : ALIM_TEMP_Pin NSS_Pin */
+  GPIO_InitStruct.Pin = ALIM_TEMP_Pin|NSS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LED1_Pin */
+  GPIO_InitStruct.Pin = LED1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : RST_Pin */
+  GPIO_InitStruct.Pin = RST_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(RST_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : DIO0_Pin DIO1_Pin */
+  GPIO_InitStruct.Pin = DIO0_Pin|DIO1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
---
+++ Core/Src/main.c
@@ -0,0 +1,382 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "spi.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "lmic.h"
+#include "hal.h"
+#include "debug.h"
+#include "math.h"
+#include "cayenne_lpp.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define VREF_MV   3300.0f
+#define ADC_MAX   4095.0f
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+// application router ID (LSBF)
+static const u1_t APPEUI[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+// unique device ID (LSBF)
+static const u1_t DEVEUI[8] = { 0x4C, 0x8F, 0x07, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
+
+// device-specific AES key (MSBF)
+static const u1_t DEVKEY[16] = { 0xA1, 0x99, 0x31, 0x2B, 0xF2, 0xD4, 0x43, 0x61,
+		0x56, 0x08, 0xBC, 0x1F, 0x51, 0x1B, 0xEA, 0x2F };
+
+static int cnt = 0;
+
+static osjob_t blinkjob;
+
+static u1_t ledstate = 0;
+
+static cayenne_lpp_t lpp;   // statique : buffer de 51 octets, pas sur la pile
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+float lm94021_adc_to_tempC(u2_t adc) {
+    // 1) code ADC -> tension en mV
+    float v = (float)adc * VREF_MV / ADC_MAX;
+
+    // 2) inversion de la parabole : 0.00176*x^2 + 5.506*x + (V - 870.6) = 0
+    const float a = 0.00176f;
+    const float b = 5.506f;
+    float c = v - 870.6f;
+
+    float disc = b*b - 4.0f*a*c;
+    if (disc < 0.0f) disc = 0.0f;        // garde-fou
+
+    float x = (-b + sqrtf(disc)) / (2.0f * a);
+
+    return x + 30.0f;                    // T = x + 30
+}
+
+static void blinkfunc(osjob_t *j) {
+// toggle LED
+	ledstate = !ledstate;
+	debug_led(ledstate);
+// reschedule blink job
+	os_setTimedCallback(j, os_getTime() + ms2osticks(100), blinkfunc);
+}
+
+// provide application router ID (8 bytes, LSBF)
+void os_getArtEui(u1_t *buf) {
+	memcpy(buf, APPEUI, 8);
+}
+// provide device ID (8 bytes, LSBF)
+void os_getDevEui(u1_t *buf) {
+	memcpy(buf, DEVEUI, 8);
+}
+// provide device key (16 bytes)
+void os_getDevKey(u1_t *buf) {
+	memcpy(buf, DEVKEY, 16);
+}
+void initsensor() {
+// Here you init your sensors
+}
+
+void initfunc(osjob_t *j) {
+	// intialize sensor hardware
+	initsensor();
+	// reset MAC state
+	LMIC_reset();
+	// start joining
+	LMIC_startJoining();
+	// init done - onEvent() callback will be invoked...
+}
+u2_t readsensor() {
+	HAL_GPIO_WritePin(ALIM_TEMP_GPIO_Port, ALIM_TEMP_Pin, 1);
+	HAL_Delay(10);	//necess
+	HAL_ADC_Start(&hadc1);
+	if (HAL_ADC_PollForConversion(&hadc1, 5) == HAL_OK) {
+		uint16_t temp_val = HAL_ADC_GetValue(&hadc1);
+		HAL_GPIO_WritePin(ALIM_TEMP_GPIO_Port, ALIM_TEMP_Pin, 0);
+		return (u2_t) temp_val;
+	}
+	HAL_GPIO_WritePin(ALIM_TEMP_GPIO_Port, ALIM_TEMP_Pin, 0);
+	return 0;
+}
+
+static osjob_t reportjob;
+// report sensor value every minute
+static void reportfunc(osjob_t *j) {
+	// read sensor
+	u2_t val = readsensor();
+	float sensor_temp = lm94021_adc_to_tempC(val);
+	debug_time();
+	debug_valfloat("Onboard sensor temp -> ", sensor_temp, 4);
+	debug_str(" °C\r\n");
+
+	// encodage Cayenne LPP
+	cayenne_lpp_reset(&lpp);
+	cayenne_lpp_add_temperature(&lpp, 1, sensor_temp);        // canal 1 : 0.1 °C, 4 octets
+	// prepare and schedule data for transmission
+	LMIC_setTxData2(1, lpp.buffer, lpp.cursor, 0);            // port 1, 8 octets, unconfirmed
+
+	// reschedule job in 15 seconds
+	os_setTimedCallback(j, os_getTime() + sec2osticks(15), reportfunc);
+}
+
+//////////////////////////////////////////////////
+// LMIC EVENT CALLBACK
+//////////////////////////////////////////////////
+void onEvent(ev_t ev) {
+	debug_event(ev);   // source unique du nom de l'event
+	switch (ev) {
+	case EV_JOINING:
+		blinkfunc(&blinkjob);
+		break;
+	case EV_JOINED:
+		// kick-off periodic sensor job
+		os_clearCallback(&blinkjob);
+		debug_led(1);
+		reportfunc(&reportjob);
+		break;
+	case EV_TXCOMPLETE:
+		if (LMIC.txrxFlags & TXRX_ACK)
+			debug_str("  -> received ack\r\n");
+		if (LMIC.dataLen) {
+			debug_val("  -> payload bytes = ", LMIC.dataLen);
+		}
+		break;
+	case EV_JOIN_FAILED:
+	case EV_SCAN_TIMEOUT:
+	case EV_BEACON_FOUND:
+	case EV_BEACON_MISSED:
+	case EV_BEACON_TRACKED:
+	case EV_RFU1:
+	case EV_REJOIN_FAILED:
+	case EV_LOST_TSYNC:
+	case EV_RESET:
+	case EV_RXCOMPLETE:
+	case EV_LINK_DEAD:
+	case EV_LINK_ALIVE:
+	case EV_TXSTART:
+		// nom deja logge par debug_event() en haut, rien a ajouter
+		break;
+	default:
+		debug_str("  -> unknown event\r\n");
+		break;
+	}
+}
+
+static void hellofunc(osjob_t *j) {
+	debug_str("Hello World!\r\n");
+
+	debug_val("cnt = ", cnt);
+
+	debug_led(++cnt & 1);
+
+	os_setTimedCallback(j, os_getTime() + sec2osticks(1), hellofunc);
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI3_Init();
+  MX_USART1_UART_Init();
+  MX_ADC1_Init();
+  MX_TIM16_Init();
+  /* USER CODE BEGIN 2 */
+	HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED);
+
+	HAL_GPIO_WritePin(ALIM_TEMP_GPIO_Port, ALIM_TEMP_Pin, 1);
+
+	HAL_TIM_Base_Start_IT(&htim16);   // <----------- change to your setup
+
+	__HAL_SPI_ENABLE(&hspi3);        // <----------- change to your setup
+
+	osjob_t initjob;
+
+	// initialize runtime env
+	os_init();
+	// initialize debug library
+	debug_init();
+	// setup initial job
+	//os_setCallback(&hellojob, hellofunc);
+
+	os_setCallback(&initjob, initfunc);
+	// execute scheduled jobs and events
+	os_runloop();
+	// (not reached)
+	return 0;
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	while (1) {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	}
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure LSE Drive Capability
+  */
+  HAL_PWR_EnableBkUpAccess();
+  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE|RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 40;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Enable MSI Auto calibration
+  */
+  HAL_RCCEx_EnableMSIPLLMode();
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
